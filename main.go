e3fcf888dbdf73feaa879d536a71f5691b05d4dc
nt2311 <nguyentoan231196@gmail.com>
2025-03-18 21:28:14 +0700

    read messages.text and slice into each 8 bytes

--- main.go
+++ main.go
@@ -1,7 +1,30 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"io"
+	"log"
+	"os"
+)
 
 func main() {
-	fmt.Println("I hope I get the job!")
+	f, err := os.Open("./messages.txt")
+	if err != nil {
+		log.Fatalf("cannot open messages file: %v", err)
+	}
+
+	defer f.Close()
+
+	buf := make([]byte, 8)
+	for {
+		n, err := f.Read(buf)
+		if err != nil {
+			if err == io.EOF {
+				break
+			}
+			log.Fatalf("cannot read 8 bytes from file: %v", err)
+		}
+
+		fmt.Printf("read: %s\n", buf[:n])
+	}
 }
